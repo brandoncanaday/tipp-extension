ea851512f9781e5e441ade9833b448879f48e30f
Brandon Canaday <brandon.canaday@yahoo.com>
2018-02-21 21:23:17 -0500

    minified all the css and js files. changed stripe client id to live. changed all backend urls to tippextension.com

--- js/show-correct-popup.js
+++
@@ -1,6 +0,0 @@
-
-(function() {
-    // redirect to dashboard popup if logged in
-    const _acc = JSON.parse((localStorage.getItem('tipp_account')) ? localStorage.getItem('tipp_account') : '{}');
-    if(_acc.jwt) window.location.replace(chrome.runtime.getURL('dashboard.html'));
-})();
